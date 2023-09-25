b0c54c47cda30606c20e79ff07cf2e65794b454e
John Wostenberg <jwosty@users.noreply.github.com>
2023-09-25 12:18:20 -0600

    Move RIDS to csproj

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ cd "$(dirname "$0")"
 
 proj="src/TestApp/TestApp.csproj"
 rid="osx-x64"
-rids="osx-x64;osx-arm64"
 cfg="Release"
 
 run() {
@@ -14,5 +13,5 @@ run() {
 }
 
 run dotnet restore $proj --runtime $rid -p "RuntimeIdentifiers=\"$rids\""
-run dotnet build $proj --configuration $cfg --no-restore --runtime $rid -p "RuntimeIdentifiers=\"$rids\"" --self-contained true
-run dotnet publish $proj --configuration $cfg --no-restore --no-build --runtime $rid -p "RuntimeIdentifiers=\"$rids\"" --self-contained true --output publish/$rid
+run dotnet build $proj --configuration $cfg --no-restore --runtime $rid --self-contained true
+run dotnet publish $proj --configuration $cfg --no-restore --no-build --runtime $rid --self-contained true --output publish/$rid
